68e8f64aa5e1022cedd56ce0fed670fe9c51090a
Harrry <oliverobiwulu158@gmail.com>
2023-03-18 06:39:28 -0700

    Comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,17 +11,20 @@ int main(void)
 
     for (num1 = 0; num1 < 10; num1++)
     {
-        for (num2 = num1; num2 < 10; num2++)
+        for (num2 = num1 + 1; num2 < 10; num2++)
         {
-            int comma = ((num1 == 9) && (num2 == 9)) ? '\n' : ',';
+            putchar(num1 + '0');
+            putchar(num2 + '0');
 
-            putchar((num1 % 10) + '0');
-            putchar((num2 % 10) + '0');
-            putchar(comma);
-            putchar(' ');
+            if (num1 != 8 || num2 != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
         }
     }
 
+    putchar('\n');
     return (0);
 }
 
