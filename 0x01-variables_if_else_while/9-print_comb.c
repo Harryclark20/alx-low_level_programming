b96c52b25207d117b5aaf260db2c23ecdbe2b8fd
Harrry <oliverobiwulu158@gmail.com>
2023-03-18 06:46:02 -0700

    Comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,25 +5,23 @@
  *
  * Return: Always 0 (Success)
  */
-
-
-int main(void) {
+int main(void)
+{
     int num1, num2;
 
-    for (num1 = 0; num1 < 10; num1++) {
-        for (num2 = num1; num2 < 10; num2++) {
-            putchar(num1 + '0');
-            putchar(num2 + '0');
+    for (num1 = 0; num1 < 10; num1++)
+    {
+        for (num2 = num1; num2 < 10; num2++)
+        {
+            int comma = ((num1 == 9) && (num2 == 9)) ? '\n' : ',';
 
-            if (num1 != 9 || num2 != 9) {
-                putchar(',');
-                putchar(' ');
-            }
+            putchar((num1 % 10) + '0');
+            putchar((num2 % 10) + '0');
+            putchar(comma);
+            putchar(' ');
         }
     }
 
-    putchar('\n');
-
-    return 0;
+    return (0);
 }
 
