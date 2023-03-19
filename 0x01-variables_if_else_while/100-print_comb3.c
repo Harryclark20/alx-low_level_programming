7629b612238d128e9dbed18bfa54f5b4d9aef8a3
Harrry <oliverobiwulu158@gmail.com>
2023-03-19 04:49:06 -0700

    Comb5

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinatio
+ * ll your code should be in the main function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar(num1 % 10 + '0');
+			putchar(num2 % 10 + '0');
+
+			if (num1 < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
