7548b2c77b6cb4a96ed758be06b4af7fa614ddf6
Harrry <oliverobiwulu158@gmail.com>
2023-04-03 03:31:15 -0700

    MEMset

---
+++ 0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main-prints putchar by a new line
+ * description
+ * return:always 0
+ */
+int main(void)
+{
+	char abc[]  = "_putchar";
+
+	int c;
+
+	for  (c = 0; c < 0; c++)
+	{
+		_putchar(abc[c]);
+	}			
+       	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+* alphabet - small letter
+* Return: All good
+*/
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
